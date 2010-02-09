6e4c6ac1a31590b0d07c6fabc58fb8b016247ede
sindre_mehus <sindre_mehus@104adb81-38d2-4886-885e-82898e39ce25>
2010-02-09 14:08:00 +0000

    Tagging 3.9.
    git-svn-id: svn://svn.code.sf.net/p/subsonic/code@1336 104adb81-38d2-4886-885e-82898e39ce25

---
+++ tags/release-3.9/subsonic-main/src/main/java/net/sourceforge/subsonic/controller/NetworkSettingsController.java
@@ -0,0 +1,103 @@
+/*
+ This file is part of Subsonic.
+
+ Subsonic is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ Subsonic is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with Subsonic.  If not, see <http://www.gnu.org/licenses/>.
+
+ Copyright 2009 (C) Sindre Mehus
+ */
+package net.sourceforge.subsonic.controller;
+
+import net.sourceforge.subsonic.command.NetworkSettingsCommand;
+import net.sourceforge.subsonic.service.NetworkService;
+import net.sourceforge.subsonic.service.SettingsService;
+import org.apache.commons.lang.ObjectUtils;
+import org.apache.commons.lang.StringUtils;
+import org.springframework.web.servlet.mvc.SimpleFormController;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Date;
+import java.util.Random;
+
+/**
+ * Controller for the page used to change the network settings.
+ *
+ * @author Sindre Mehus
+ */
+public class NetworkSettingsController extends SimpleFormController {
+
+    private static final long TRIAL_DAYS = 30L;
+
+    private SettingsService settingsService;
+    private NetworkService networkService;
+
+    protected Object formBackingObject(HttpServletRequest request) throws Exception {
+
+        if (request.getParameter("test") != null) {
+            networkService.testUrlRedirection();
+        }
+
+        NetworkSettingsCommand command = new NetworkSettingsCommand();
+        command.setPortForwardingEnabled(settingsService.isPortForwardingEnabled());
+        command.setPortForwardingPublicPort(settingsService.getPortForwardingPublicPort());
+        command.setUrlRedirectionEnabled(settingsService.isUrlRedirectionEnabled());
+        command.setUrlRedirectFrom(settingsService.getUrlRedirectFrom());
+
+        Date trialExpires = settingsService.getUrlRedirectTrialExpires();
+        command.setTrialExpires(trialExpires);
+        command.setTrialExpired(trialExpires != null && trialExpires.before(new Date()));
+        command.setTrial(trialExpires != null && !settingsService.isLicenseValid());
+
+        return command;
+    }
+
+    protected void doSubmitAction(Object cmd) throws Exception {
+        NetworkSettingsCommand command = (NetworkSettingsCommand) cmd;
+
+        if (command.isPortForwardingEnabled() != settingsService.isPortForwardingEnabled() ||
+            command.getPortForwardingPublicPort() != settingsService.getPortForwardingPublicPort()) {
+            settingsService.setPortForwardingEnabled(command.isPortForwardingEnabled());
+            settingsService.setPortForwardingPublicPort(command.getPortForwardingPublicPort());
+            settingsService.save();
+            networkService.initPortForwarding();
+            networkService.initUrlRedirection(); // Necessary to reflect changed port number.
+        }
+
+        if (command.isUrlRedirectionEnabled() != settingsService.isUrlRedirectionEnabled() ||
+            !ObjectUtils.equals(command.getUrlRedirectFrom(), settingsService.getUrlRedirectFrom())) {
+            settingsService.setUrlRedirectionEnabled(command.isUrlRedirectionEnabled());
+            settingsService.setUrlRedirectFrom(StringUtils.lowerCase(command.getUrlRedirectFrom()));
+
+            if (!settingsService.isLicenseValid() && settingsService.getUrlRedirectTrialExpires() == null) {
+                Date expiryDate = new Date(System.currentTimeMillis() + TRIAL_DAYS * 24L * 3600L * 1000L);
+                settingsService.setUrlRedirectTrialExpires(expiryDate);
+            }
+
+            if (settingsService.getServerId() == null) {
+                Random rand = new Random(System.currentTimeMillis());
+                settingsService.setServerId(String.valueOf(Math.abs(rand.nextLong())));
+            }
+
+            settingsService.save();
+            networkService.initUrlRedirection();
+        }
+    }
+
+    public void setSettingsService(SettingsService settingsService) {
+        this.settingsService = settingsService;
+    }
+
+    public void setNetworkService(NetworkService networkService) {
+        this.networkService = networkService;
+    }
+}
---
+++ tags/release-3.9/subsonic-site/src/main/webapp/screenshots.jsp
@@ -0,0 +1,47 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+        "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<%! String current = "screenshots"; %>
+<%@ include file="header.jsp" %>
+
+<body>
+
+<a name="top"/>
+
+<div id="container">
+    <%@ include file="menu.jsp" %>
+
+    <div id="content">
+        <div id="main-col">
+            <h1>Subsonic Screenshots</h1>
+            <a href="inc/img/screenshots/screen02.png"><img src="inc/img/screenshots/thumb02.png" alt="" style="padding:3px"/></a>
+            <a href="inc/img/screenshots/screen01.png"><img src="inc/img/screenshots/thumb01.png" alt="" style="padding:3px"/></a>
+            <a href="inc/img/screenshots/screen05.png"><img src="inc/img/screenshots/thumb05.png" alt="" style="padding:3px"/></a>
+            <a href="inc/img/screenshots/screen04.png"><img src="inc/img/screenshots/thumb04.png" alt="" style="padding:3px"/></a>
+            <a href="inc/img/screenshots/screen06.png"><img src="inc/img/screenshots/thumb06.png" alt="" style="padding:3px"/></a>
+            <a href="inc/img/screenshots/screen03.png"><img src="inc/img/screenshots/thumb03.png" alt="" style="padding:3px"/></a>
+            <a href="inc/img/screenshots/screen07.png"><img src="inc/img/screenshots/thumb07.png" alt="" style="padding:3px"/></a>
+            <a href="inc/img/screenshots/screen08.png"><img src="inc/img/screenshots/thumb08.png" alt="" style="padding:3px"/></a>
+            <a href="inc/img/screenshots/screen09.png"><img src="inc/img/screenshots/thumb09.png" alt="" style="padding:3px"/></a>
+            <a href="inc/img/screenshots/screen10.png"><img src="inc/img/screenshots/thumb10.png" alt="" style="padding:3px"/></a>
+            <a href="inc/img/screenshots/screen11.png"><img src="inc/img/screenshots/thumb11.png" alt="" style="padding:3px"/></a>
+            <a href="inc/img/screenshots/screen12.png"><img src="inc/img/screenshots/thumb12.png" alt="" style="padding:3px"/></a>
+            <a href="inc/img/screenshots/screen13.png"><img src="inc/img/screenshots/thumb13.png" alt="" style="padding:3px"/></a>
+            <object width="320" height="265"><param name="movie" value="http://www.youtube.com/v/Va9DtFeG7cA&hl=en&fs=1&"></param><param name="allowFullScreen" value="true"></param><param name="allowscriptaccess" value="always"></param><embed src="http://www.youtube.com/v/Va9DtFeG7cA&hl=en&fs=1&" type="application/x-shockwave-flash" allowscriptaccess="always" allowfullscreen="true" width="320" height="265"></embed></object>
+        </div>
+
+        <div id="side-col">
+            <%@ include file="download-subsonic.jsp" %>
+            <%@ include file="donate.jsp" %>
+        </div>
+
+        <div class="clear">
+        </div>
+    </div>
+    <hr/>
+    <%@ include file="footer.jsp" %>
+</div>
+
+
+</body>
+</html>
