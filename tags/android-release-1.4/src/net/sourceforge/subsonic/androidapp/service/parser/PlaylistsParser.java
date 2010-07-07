891d29fda8e3df5f3eb57a12c97dbbc3f5070318
sindre_mehus <sindre_mehus@104adb81-38d2-4886-885e-82898e39ce25>
2010-07-07 11:30:37 +0000

    Tagging Android 1.4
    git-svn-id: svn://svn.code.sf.net/p/subsonic/code@1673 104adb81-38d2-4886-885e-82898e39ce25

---
+++ tags/android-release-1.4/src/net/sourceforge/subsonic/androidapp/service/parser/PlaylistsParser.java
@@ -0,0 +1,71 @@
+/*
+ This file is part of Subsonic.
+
+ Subsonic is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ Subsonic is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with Subsonic.  If not, see <http://www.gnu.org/licenses/>.
+
+ Copyright 2009 (C) Sindre Mehus
+ */
+package net.sourceforge.subsonic.androidapp.service.parser;
+
+import android.content.Context;
+import android.util.Xml;
+import net.sourceforge.subsonic.androidapp.R;
+import net.sourceforge.subsonic.androidapp.domain.Playlist;
+import net.sourceforge.subsonic.androidapp.util.ProgressListener;
+import org.xmlpull.v1.XmlPullParser;
+
+import java.io.Reader;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author Sindre Mehus
+ */
+public class PlaylistsParser extends AbstractParser {
+
+    public PlaylistsParser(Context context) {
+        super(context);
+    }
+
+    public List<Playlist> parse(Reader reader, ProgressListener progressListener) throws Exception {
+        if (progressListener != null) {
+            progressListener.updateProgress(R.string.parser_reading);
+        }
+
+        XmlPullParser parser = Xml.newPullParser();
+        parser.setInput(reader);
+
+        List<Playlist> result = new ArrayList<Playlist>();
+        int eventType;
+        do {
+            eventType = parser.next();
+            if (eventType == XmlPullParser.START_TAG) {
+                String tag = parser.getName();
+                if ("playlist".equals(tag)) {
+                    String name = get(parser, "name");
+                    String id = get(parser, "id");
+                    result.add(new Playlist(id, name));
+                } else if ("error".equals(tag)) {
+                    handleError(parser);
+                }
+            }
+        } while (eventType != XmlPullParser.END_DOCUMENT);
+
+        if (progressListener != null) {
+            progressListener.updateProgress(R.string.parser_reading_done);
+        }
+        return result;
+    }
+
+}
